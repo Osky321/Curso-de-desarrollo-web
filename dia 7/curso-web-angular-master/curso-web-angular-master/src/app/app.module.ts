ada0f219495ccb5a7fa651c98e01cdf608bbc65c
Osky321 <oscachov88@gmail.com>
2017-07-26 21:43:28 -0400

    anadir dia 7

---
+++ dia 7/curso-web-angular-master/curso-web-angular-master/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+
+import { MdToolbarModule } from "@angular/material";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
